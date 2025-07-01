e1dadc4130ca0306ce26b1399c2bf16912084169
Udaya Somanna <udayas072001@gmail.com>
2025-07-01 11:10:53 +0530

    Tech Stack Initialization FrountEnd and BackEnd setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+// components/Navbar.jsx
+export default function Navbar() {
+  return (
+    <nav className="bg-white px-6 py-4 shadow-md flex justify-between items-center">
+      <h1 className="text-xl font-bold text-blue-600">🎓 EduStream</h1>
+    </nav>
+  );
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+// pages/Home.jsx
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-4">Welcome to EduStream 📺</h2>
+      <p>Start watching or uploading educational videos.</p>
+    </div>
+  );
+}
